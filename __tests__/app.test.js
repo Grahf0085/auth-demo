e1fdebe0447a5aa42b6bbb05250a9cf12592502c
Tucker <tuckerhoog@tutanota.com>
2021-06-29 20:57:46 -0400

    sign up user post test fail

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,28 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+const agent = request.agent(app);
+
+describe('user routes', () => {
+
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('signs up a user via POST', async () => {
+
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'tuckerhoog@tutanota.com',
+        password: 'p@ssword'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      email: 'tuckerhoog@tutanota.com'
+    });
+  });
+  
 });
+
