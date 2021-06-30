d89fbc976ad45ff209807c601702197ae8524510
Tucker <tuckerhoog@tutanota.com>
2021-06-30 12:16:20 -0400

    add profile photo

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,18 +18,20 @@ describe('user routes', () => {
       .post('/api/v1/auth/signup')
       .send({
         email: 'tuckerhoog@tutanota.com',
-        password: 'password'
+        password: 'password',
+        profilePhotoUrl: 'https://us-browse.startpage.com/av/anon-image?piurl=https%3A%2F%2Fi.ibb.co%2FHh5QNsQ%2FWiseman.png&sp=1625069635Tc55a9e659f1c58353e1c991d4fc8177e769586ea4a7948daa9fd9b2a349c3c0b'
       });
 
     expect(res.body).toEqual({
       id: '1',
-      email: 'tuckerhoog@tutanota.com'
+      email: 'tuckerhoog@tutanota.com',
+      profilePhotoUrl: 'https://us-browse.startpage.com/av/anon-image?piurl=https%3A%2F%2Fi.ibb.co%2FHh5QNsQ%2FWiseman.png&sp=1625069635Tc55a9e659f1c58353e1c991d4fc8177e769586ea4a7948daa9fd9b2a349c3c0b'
     });
   });
 
   it('signs in a user via POST', async () => {
 
-    await UserService.create({ email: 'tuckerhoog@tutanota.com', password: 'password' }); 
+    await UserService.create({ email: 'tuckerhoog@tutanota.com', password: 'password', profilePhotoUrl: 'https://us-browse.startpage.com/av/anon-image?piurl=https%3A%2F%2Fi.ibb.co%2FHh5QNsQ%2FWiseman.png&sp=1625069635Tc55a9e659f1c58353e1c991d4fc8177e769586ea4a7948daa9fd9b2a349c3c0b' }); 
 
     const res = await request(app)
       .post('/api/v1/auth/login')
@@ -40,7 +42,9 @@ describe('user routes', () => {
 
     expect(res.body).toEqual({
       id: '1',
-      email: 'tuckerhoog@tutanota.com'
+      email: 'tuckerhoog@tutanota.com',
+      profilePhotoUrl: 'https://us-browse.startpage.com/av/anon-image?piurl=https%3A%2F%2Fi.ibb.co%2FHh5QNsQ%2FWiseman.png&sp=1625069635Tc55a9e659f1c58353e1c991d4fc8177e769586ea4a7948daa9fd9b2a349c3c0b'
+
     });
 
   });
