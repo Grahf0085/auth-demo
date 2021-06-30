f2aed54e918308311d558c87ea0b32599b4ed261
Tucker <tuckerhoog@tutanota.com>
2021-06-30 01:16:13 -0400

    login test pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import UserService from '../lib/services/UserService.js';
 
 // const agent = request.agent(app);
 
@@ -24,6 +25,24 @@ describe('user routes', () => {
       id: '1',
       email: 'tuckerhoog@tutanota.com'
     });
+  });
+
+  it('signs in a user via POST', async () => {
+
+    await UserService.create({ email: 'tuckerhoog@tutanota.com', password: 'password' }); 
+
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'tuckerhoog@tutanota.com',
+        password: 'password'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      email: 'tuckerhoog@tutanota.com'
+    });
+
   });
 
 });
